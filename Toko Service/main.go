32f119416233921002e02639c562c82495e0de08
unknown <bayuxbca@gmail.com>
2023-09-10 15:44:33 +0700

    feat: func get detail toko

--- Toko Service/main.go
+++ Toko Service/main.go
@@ -1,7 +1,19 @@
 package main
 
-func getDetailToko() {
+import (
+	"encoding/json"
+	"net/http"
+)
 
+func getDetailToko(w http.ResponseWriter, r http.Request) {
+	// already hit data from DB, get detail toko
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"name":           "Byyy Store",
+		"total_products": "100",
+	})
 }
 
 func getAllToko() {
